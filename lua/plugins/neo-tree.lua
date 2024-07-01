a42cfe0a3dcf5a8b04bb3932dfd86eccc2fa72e1
donghua <997726077@qq.com>
2024-07-01 11:55:20 +0800

    neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,13 +11,44 @@ return {
     keys = {
         { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
     },
+    opts = {
+        event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              -- auto close
+              -- vimc.cmd("Neotree close")
+              -- OR
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+        },
+
+        window = {
+            mappings = {
+            ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
+            ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
+            ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+        },
+  },
+    },
     init = function()
         vim.g.loaded_netrw       = 1
 	    vim.g.loaded_netrwPlugin = 1
         local stats = vim.uv.fs_stat(vim.fn.argv(0))
         if stats and stats.type == "directory" then
             -- require("neo-tree").toggle()
-            vim.cmd("Neotree")
+            vim.api.nvim_create_autocmd("VimEnter", {
+            callback = function()
+                -- 检查打开的路径是否是目录
+                local stats = vim.uv.fs_stat(vim.fn.argv(0))
+                if stats and stats.type == "directory" then
+                    vim.cmd[[Neotree]]
+
+                end
+            end,
+            once = true
+        })
         end
     end,
 }
