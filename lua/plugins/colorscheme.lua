52a9a973ce9d64f15297ca922713ce04446587e8
donghua <997726077@qq.com>
2024-07-05 14:46:05 +0800

    colorschem

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        vim.cmd[[colorscheme tokyonight-storm]]
-    end
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd[[colorscheme tokyonight-storm]]
+        end
+    },
+    { "catppuccin/nvim", name = "catppuccin"}
 }
